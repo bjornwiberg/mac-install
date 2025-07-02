289afa855b84b8e59a93c30fa4aef0cc804ebb37
Björn Wiberg <b@bjrn.nu>
2025-07-02 17:12:26 +0200

    feat: add gitsigns.nvim for enhanced Git integration in Neovim

--- dotfiles/nvim/lua/plugins/git.lua
+++ dotfiles/nvim/lua/plugins/git.lua
@@ -2,8 +2,22 @@ return function(use)
   -- Git plugin definitions
   use 'tpope/vim-fugitive'
   use 'TimUntersberger/neogit'
-  use 'airblade/vim-gitgutter'
   use 'jreybert/vimagit'
+  use {
+    'lewis6991/gitsigns.nvim',
+    requires = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      require('gitsigns').setup {
+        current_line_blame = true,
+        current_line_blame_opts = {
+          virt_text = true,
+          virt_text_pos = 'eol',
+          delay = 500,
+          ignore_whitespace = false,
+        },
+      }
+    end
+  }
 
   -- Git Plugin Configurations
   -- Git commit log mappings
